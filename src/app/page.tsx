dac92e8ce684bb540da5d5d88938fbbd72e5e86d
TUAN NGUYEN <tuannguyen1702@gmail.com>
2024-03-16 11:26:02 +0700

    fix bug to build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <div>
+    <div className="flex">
+      <div className="flex-1">hh</div>
+      <div className="w-[200px]">ss</div>
+      {/* <div>
         <div className="rounded-lg z-[1] overflow-hidden shadow-lg border-gray-50 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -31,6 +33,9 @@ export default function Home() {
             Tham gia{" "}
             <span className="absolute text text-shadow-default text-[50px] md:text-[70px] text-blue-500 -bottom-2 right-4 drop-shadow-3xl">
               23
+            </span>
+          </div>
+          <div className="relative rounded-lg px-4 py-2 md:p-4  bg-red-100 text-left">
             Xin vắng{" "}
             <span className="absolute text text-shadow-default text-[50px] md:text-[70px] text-red-500 -bottom-2 right-4 drop-shadow-3xl">
               13
@@ -49,7 +54,8 @@ export default function Home() {
             Chi tiết bình chọn
           </Link>
         </div>
-      </div>
-    </>
+      </div> */}
+      
+    </div>
   );
 }
