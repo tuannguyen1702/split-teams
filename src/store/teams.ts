369c5a41405c2ba60a1431f810194ab5e3ddd2f8
ICT-PC5\Tuan - Bstart <tuan.nguyen.q@bstarsolutions.com>
2024-03-19 14:54:53 +0700

    update goal keeper

--- src/store/teams.ts
+++ src/store/teams.ts
@@ -15,13 +15,13 @@ const data = localStorageGetItem("data")
         "Lâm Đạt",
       ],
       [
-        "Thầy Huấn <span class='ml-1 dot-red inline-flex'>+</span>",
+        "Thầy Huấn <span class='ml-1 dot-red inline-flex'>+</span><span class='ml-1 dot-blue inline-flex'>G</span>",
         "Lê Ku <span class='ml-1 dot-red inline-flex'>+</span>",
-        "Lê Đức",
+        "Lê Đức <span class='ml-1 dot-blue inline-flex'>G</span>",
         "Tuấn Phú Yên ",
       ],
-      ["Bảo Ford", "A Thái", "Hiếu ", "Đàn"],
-      ["Thầy Trình", "Tuấn Nguyễn ", "A Khôi ", "Thành Neymar"],
+      ["Bảo Ford <span class='ml-1 dot-blue inline-flex'>G</span>", "A Thái", "Hiếu ", "Đàn <span class='ml-1 dot-blue inline-flex'>G</span>"],
+      ["Thầy Trình", "Tuấn Nguyễn <span class='ml-1 dot-blue inline-flex'>G</span>", "A Khôi <span class='ml-1 dot-blue inline-flex'>G</span>", "Thành Neymar"],
       [
         "Tho <span class='ml-1 dot-red inline-flex'>+</span>",
         "Quyền <span class='ml-1 dot-red inline-flex'>+</span>",
@@ -36,7 +36,7 @@ const data = localStorageGetItem("data")
       ],
       ["Chung VCB", "Duy", "Ngọc Anh", "Tâm Vũ"],
       ["A Tân", "A Hải ", "Luis", "Bùi Thiện"],
-      ["A Lành", "Sang", "A Chung", "A Hoàng "],
+      ["A Lành", "Sang", "A Chung <span class='ml-1 dot-blue inline-flex'>G</span>", "A Hoàng <span class='ml-1 dot-blue inline-flex'>G</span>"],
     ];
 
 type TeamState = {
