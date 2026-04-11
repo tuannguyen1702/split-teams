1dfec7d7870f0b4a0e6d480fac8b92b0476f8e56
tuannguyen1702 <tuannguyen1702@gmail.com>
2026-04-11 14:16:46 +0700

    Update MemberItem.tsx

--- src/app/_components/MemberItem.tsx
+++ src/app/_components/MemberItem.tsx
@@ -6,7 +6,7 @@ interface MemberItemProps extends MemberItemData {}
 export default function MemberItem(props: Readonly<MemberItemProps>) {
   const { index, id, name} = props;
   return (
-    <div className="flex rounded-sm bg-white text-left mt-2 py-2 px-2 md:px-4">
+    <div className="flex rounded-sm bg-white text-left mt-2 py-1 px-2 md:px-4">
         <div className="w-[22px]">{index}</div>
       <SafeHTMLRender id={id} isStyleContent htmlContent={name} className="flex items-center" />
       {/* <div>{isPlus ? "+" : ""}</div> */}
