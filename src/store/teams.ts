57944679bf9988f40074143e063a2d1df12d0235
tuannguyen1702 <tuannguyen1702@gmail.com>
2026-04-26 20:41:18 +0700

    Update teams.ts

--- src/store/teams.ts
+++ src/store/teams.ts
@@ -8,18 +8,11 @@ const time = localStorageGetItem("time")
   : 0;
 
 const data = [
-  ["Ngoc Anh", "Chung VCB"],
-  ["Hải", "Tùng Thái"],
-  ["Anh Tho", "Thuan Ngoc"],
-  ["Lê Tín", "Nc Quyền"],
-  ["Nguyễn Văn Lợi <span class='ml-1 dot-blue inline-flex'>*</span>", "Dinh Bui"],
-  ["Minh Khôi + Louis", "Duy + A Lanh"],
-  ["Quang Luân", "Quang Tuấn"],
-  ["Nguyen Thanh", "Lê Vũ"],
-  ["A Hoàng", "Quốc Bảo"],
-  ["Tmthành", "Học"],
-    ["A Dung + Bin", "Tuan PY + Ken"],
-  ["Thành", "Tuan Nguyen <span class='ml-1 dot-blue inline-flex'>*</span>"]
+
+  ["Quang <span class='ml-1 dot-blue inline-flex'>*</span>", "Lương <span class='ml-1 dot-blue inline-flex'>*</span>"],
+  ["Sỹ", "Thái <span class='ml-1 dot-blue inline-flex'>*</span>", "Cang <span class='ml-1 dot-blue inline-flex'>*</span>"],
+  ["Hải", "Vương","Tân"],
+
 ];
 
 type TeamState = {
@@ -34,8 +27,8 @@ type TeamState = {
 export const useTeamStore = create<TeamState>((set) => ({
   data: data,
   time: time,
-  totalTeam: 2,
-  totalTeamMember: 12,
+  totalTeam: 3,
+  totalTeamMember: 3,
   setTotalTeam: (value: number) => set({ totalTeam: value }),
   setTotalTeamMember: (value: number) => set({ totalTeamMember: value }),
 }));
