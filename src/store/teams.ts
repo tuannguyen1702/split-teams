33bc4e6b915491a53ebbf328392ebec901a8acd1
tuannguyen1702 <tuannguyen1702@gmail.com>
2026-08-25 10:44:14 +0700

    Update teams.ts

--- src/store/teams.ts
+++ src/store/teams.ts
@@ -8,11 +8,58 @@ const time = localStorageGetItem("time")
   : 0;
 
 const data = [
-
-  ["Quang <span class='ml-1 dot-blue inline-flex'>*</span>", "Lương <span class='ml-1 dot-blue inline-flex'>*</span>", "Bình"],
-  ["Sỹ", "Thái <span class='ml-1 dot-blue inline-flex'>*</span>", "Cang <span class='ml-1 dot-blue inline-flex'>*</span>"],
-  ["Hải", "Vương","Tân"],
-
+  [
+    'Quang Tuấn',
+    'Lê Ku',
+    'Trịnh Thành',
+    'Du Nguyễn',
+    'Thái Bùi',
+    'Anh Tho',
+    'Nguyễn Văn Lợi',
+    'Võ Văn Vương',
+    'Anh Lê Ngọc',
+    'Lanh Nguyen',
+    'Trần Đào',
+  ],
+  [
+    'Quang Luân',
+    'Nguyễn Thành',
+    'Caophong Nguyen',
+    'Bùi Thiện',
+    'Tuan Nguyen',
+    'Nc Quyền',
+    'Phùng Vĩnh Phú',
+    '------',
+    'Anh Chung VCB',
+    'Lã Nguyen Thành Sang',
+    'Anh Hải SV',
+  ],
+  [
+    'Thủ môn bạn Bin',
+    'Lê Tín',
+    'Tâm Tít',
+    'Nguyễn Minh Khôi',
+    '------',
+    'Thuan Ngoc',
+    'Trần Quang',
+    'Tin Nguyen',
+    'Hoàng Duy',
+    'Thầy Trình-trí Việt-ielts Master',
+    'Louis',
+  ],
+  [
+    'TM - NEW',
+    'Ford Truyền',
+    "Huynh Thanh Tuan + Ken <span class='ml-1 dot-blue inline-flex'>*</span>",
+    'Nguyễn Đàn',
+    'Lâm Đạt',
+    "Jackie Dương <span class='ml-1 dot-blue inline-flex'>*</span>",
+    'Đinh Bùi',
+    "Khúc Ngọc Dũng + Bin <span class='ml-1 dot-blue inline-flex'>*</span>",
+    "Út Nhỏ <span class='ml-1 dot-blue inline-flex'>*</span>",
+    'A Hoàng MPFC',
+    'Thành Neymar',
+  ],
 ];
 
 type TeamState = {
@@ -27,8 +74,8 @@ type TeamState = {
 export const useTeamStore = create<TeamState>((set) => ({
   data: data,
   time: time,
-  totalTeam: 3,
-  totalTeamMember: 3,
+  totalTeam: 4,
+  totalTeamMember: 11,
   setTotalTeam: (value: number) => set({ totalTeam: value }),
   setTotalTeamMember: (value: number) => set({ totalTeamMember: value }),
 }));
